fbdfef77627b026b5e8ce06c78e9c4b4d2478f56
Dillon Barnes <dillonb07dev@gmail.com>
2022-11-25 18:43:37 +0000

    Add query commands

--- bot.py
+++ bot.py
@@ -2,25 +2,27 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 from dotenv import load_dotenv
+from gql import gql, Client
+from gql.transport.aiohttp import AIOHTTPTransport
 from utils import create_embed, handle_error
 
 import os
 import sys
 import time
 import random
+import subprocess
 import json
 
 FLOOP_CHANNELS = [
-    [1032000304343961630, False],
+    [1032000304343961630, True],
     [1031690140025880660, True],
     [1032305788494037082, True],
 ]
 
 load_dotenv()
-cogs = ["cogs.modrepls"]
 
 
-class Bot(commands.Bot):  # cogs :eyes:
+class Bot(commands.Bot):
     def __init__(self):
         intents = discord.Intents.default()
         intents.message_content = True
@@ -35,6 +37,7 @@ class Bot(commands.Bot):  # cogs :eyes:
 
 
 bot = Bot()
+client = bot
 
 
 @bot.event
@@ -52,7 +55,7 @@ async def on_ready():
 @bot.hybrid_command(
     name="restart", with_app_command=True, description="Restart the bot"
 )
-@commands.has_permissions(administrator=True)
+@commands.has_role(1045408918916055179)
 async def restart(ctx):
     await ctx.defer(ephemeral=True)
     if not ctx.author.guild_permissions.administrator:
@@ -138,8 +141,13 @@ async def floop(ctx, user: discord.Member, amount: int = 10):
                 await user.send(f"FLOOP #{i + 1} - {ctx.author.name} flooped you!")
             except:
                 if blocked == False:
-                    await ctx.author.send(embed=await create_embed(title='Oh no!', description=f'{user.mention} has blocked me. I am unable to floop them in DMs and have resorted to channels'))
-                blocked = True
+                    await ctx.author.send(
+                        embed=await create_embed(
+                            title="Oh no!",
+                            description=f"{user.mention} has blocked me. I am unable to floop them in DMs and have resorted to channels",
+                        )
+                    )
+                    blocked = True
                 channels = [c[0] for c in FLOOP_CHANNELS]
                 channel = bot.get_channel(random.choice(channels))
                 time.sleep(random.randint(0, 27))
@@ -154,7 +162,84 @@ async def floop(ctx, user: discord.Member, amount: int = 10):
     await ctx.reply(f"Flooped {user.mention} {amount} times!")
 
 
+@bot.hybrid_command(
+    with_app_command=True,
+    name="exec_gql",
+    description="Execute a GraphQL request.",
+)
+@commands.has_role(1045408918916055179)
+async def exec_gql(ctx, *, query: str):
+    await ctx.defer(ephemeral=False)
+    transport = AIOHTTPTransport(
+        url="https://9abe713f-fe43-4eaf-9e93-ccaf2807f9d4.id.repl.co/graphql"
+    )
+    async with Client(transport=transport) as client:
+        context = gql(query)
+        data = await client.execute(context)
+        return await ctx.reply(
+            embed=await create_embed(
+                title="Response",
+                description=f"""```json
+{json.dumps(data, sort_keys=True, indent=4, separators=(",", ": "))}
+```""",
+            )
+        )
+
+
+@bot.hybrid_command(
+    with_app_command=True,
+    name="list_all_repls",
+    description="List all repls in the database",
+)
+@commands.has_role(1045408918916055179)
+async def list_all_repls(ctx):
+    await ctx.defer(ephemeral=False)
+    transport = AIOHTTPTransport(
+        url="https://9abe713f-fe43-4eaf-9e93-ccaf2807f9d4.id.repl.co/graphql"
+    )
+    async with Client(transport=transport) as client:
+        query = gql(
+            """
+    {
+    domains {
+        name
+        owner
+    }
+    }
+    """
+        )
+        data = await client.execute(query)
+        return await ctx.reply(
+            embed=await create_embed(
+                title="Data",
+                description=f"""```json
+{json.dumps(data['domains'], sort_keys=True, indent=4, separators=(",", ": "))}
+```""",
+            )
+        )
+
+
+@bot.hybrid_command(with_app_command=True, name="exec", description="Execute a command")
+@commands.has_role(1045408918916055179)
+async def exec(ctx, *, command: str):
+    await ctx.defer(ephemeral=False)
+    response = subprocess.run(
+        command, shell=True, capture_output=True, text=True, timeout=10
+    )
+    if response.returncode == 0:
+        return await ctx.reply(
+            embed=await create_embed(
+                title=f"Output for {command}",
+                description=f"""```bash
+{response.stdout}
+```""",
+            )
+        )
+    else:
+        return await ctx.reply(embed=await create_embed(title=f'Oops!', description='Sorry, something went wrong!'))
+
+
 try:
     bot.run(os.environ["BOT_TOKEN"])
 except Exception as e:
-    print(f"ERROR WITH LOGING IN: {e}")
+    print(f"ERROR WITH LOGGING IN: {e}")
--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 import discord
 from discord.ext import commands
+from gql.transport import exceptions as GQLExceptions
+import json
+
 
 async def create_embed(
     title="Command failed",
@@ -10,14 +13,30 @@ async def create_embed(
     embed = discord.Embed(title=title, description=description, color=color, **kwargs)
     return embed
 
+
 async def handle_error(ctx, error, ephemeral=True):
     if isinstance(error, commands.CommandOnCooldown):
         await ctx.reply(
             embed=await create_embed(
-                description="You're on cooldown for {:.1f}s".format(error.retry_after), ephemeral=ephemeral
+                description="You're on cooldown for {:.1f}s".format(error.retry_after),
+                ephemeral=ephemeral,
             )
         )
     elif isinstance(error, commands.DisabledCommand):
-        await ctx.reply(embed=await create_embed(description='This command is disabled.'), ephemeral=ephemeral)
+        await ctx.reply(
+            embed=await create_embed(description="This command is disabled."),
+            ephemeral=ephemeral,
+        )
+    elif isinstance(error, GQLExceptions.TransportQueryError):
+        data = json.dumps(
+            error.message, sort_keys=True, indent=4, separators=(",", ": ")
+        )
+        await ctx.reply(
+            embed=await create_embed(
+                description=f"There was an error while attempting this query:\n```json\n{data}\n```"
+            )
+        )
     else:
-        await ctx.reply(embed=await create_embed(description=error), ephemeral=ephemeral)
+        await ctx.reply(
+            embed=await create_embed(description=error), ephemeral=ephemeral
+        )
